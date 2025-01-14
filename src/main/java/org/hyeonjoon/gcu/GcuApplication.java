39ee730ebd5ca980bb28af92cfbfbb219cd5aa24
hyeonjoonpark <pjjoon1379@gmail.com>
2025-01-14 11:58:53 +0900

    project setting

---
+++ src/main/java/org/hyeonjoon/gcu/GcuApplication.java
@@ -0,0 +1,13 @@
+package org.hyeonjoon.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hyeonjoon/gcu/GcuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeonjoon.gcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
