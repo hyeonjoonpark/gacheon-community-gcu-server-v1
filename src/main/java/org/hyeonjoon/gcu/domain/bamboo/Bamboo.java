5f7ca785151225b4553dc3b1e223dacfda873656
hyeonjoonpark <pjjoon1379@gmail.com>
2025-01-16 10:37:32 +0900

    #4 Bamboo 엔티티에 필요한 컬럼 추가 및 대나무 숲 댓글 BambooComment 엔티티 추가

--- src/main/java/org/hyeonjoon/gcu/domain/bamboo/Bamboo.java
+++ src/main/java/org/hyeonjoon/gcu/domain/bamboo/Bamboo.java
@@ -5,7 +5,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hyeonjoon.gcu.domain.bamboo_comment.BambooComment;
 import org.hyeonjoon.gcu.global.common.AuditingFields;
+
+import java.util.List;
 
 @Entity @Table(name = "tbl_bamboo") @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,12 +17,29 @@ public class Bamboo extends AuditingFields {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 50) private String title;
-    @Column(length = 200) private String content;
+    @Column(length = 50, nullable = false) private String title;
+    @Column(length = 200, nullable = false) private String content;
+    @Column(nullable = false) private boolean isAnonymous;
+    @Column(nullable = false) @ColumnDefault("0") private int likeCount;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "bamboo")
+    private List<BambooComment> comments;
+
+    public void addBambooComment(BambooComment comment) {
+        comments.add(comment);
+        comment.setBamboo(this);
+    }
+
+    public void removeBambooComment(BambooComment comment) {
+        comments.remove(comment);
+        comment.setBamboo(null);
+    }
 
     @Builder
-    public Bamboo(String title, String content) {
+    public Bamboo(String title, String content, boolean isAnonymous, int likeCount) {
         this.title = title;
         this.content = content;
+        this.isAnonymous = isAnonymous;
+        this.likeCount = likeCount;
     }
 }
---
+++ src/main/java/org/hyeonjoon/gcu/domain/bamboo_comment/BambooComment.java
@@ -0,0 +1,24 @@
+package org.hyeonjoon.gcu.domain.bamboo_comment;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hyeonjoon.gcu.domain.bamboo.Bamboo;
+import org.hyeonjoon.gcu.global.common.AuditingFields;
+
+@Entity @Table(name = "tbl_bamboo_comment") @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BambooComment extends AuditingFields {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50) private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY) @Setter
+    private Bamboo bamboo;
+
+    @Builder
+    public BambooComment(String comment, Bamboo bamboo) {
+        this.comment = comment;
+        this.bamboo = bamboo;
+    }
+}
