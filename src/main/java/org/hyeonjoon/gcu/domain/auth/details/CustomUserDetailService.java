1eaa595f3d6d18a0d71b33ae21e39f7694c39940
hyeonjoonpark <pjjoon1379@gmail.com>
2025-01-17 10:20:55 +0900

    #10 SecurityDetails 구현
    - CustomUserDetails
    - CustomUserDetailService

---
+++ src/main/java/org/hyeonjoon/gcu/domain/auth/details/CustomUserDetailService.java
@@ -0,0 +1,29 @@
+package org.hyeonjoon.gcu.domain.auth.details;
+
+import lombok.RequiredArgsConstructor;
+import org.hyeonjoon.gcu.domain.auth.repository.UserRepository;
+import org.hyeonjoon.gcu.domain.user.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username + ": " + "찾을 수 없는 사용자입니다");
+        }
+        return new CustomUserDetails(user, new HashMap<>());
+    }
+
+    public UserDetails loadUserById(String id) throws UsernameNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException(id + ": " + "찾을 수 없는 사용자입니다"));
+        return new CustomUserDetails(user, new HashMap<>());
+    }
+}
---
+++ src/main/java/org/hyeonjoon/gcu/domain/auth/details/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package org.hyeonjoon.gcu.domain.auth.details;
+
+import lombok.RequiredArgsConstructor;
+import org.hyeonjoon.gcu.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+    private final Map<String, Object> attributes;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
