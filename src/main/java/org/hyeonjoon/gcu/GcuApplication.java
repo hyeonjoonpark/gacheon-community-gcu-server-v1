632610557c38f6ef644989e2ef7669b3384b5420
hyeonjoonpark <pjjoon1379@gmail.com>
2025-01-17 10:20:08 +0900

    #8 JpaAuditing 설정
    - createdBy 사용을 위해 AuditorAware 을 상속받아서 구현하였습니다

--- src/main/java/org/hyeonjoon/gcu/GcuApplication.java
+++ src/main/java/org/hyeonjoon/gcu/GcuApplication.java
@@ -2,8 +2,10 @@ package org.hyeonjoon.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GcuApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/hyeonjoon/gcu/global/security/auditing/AuditingAware.java
@@ -0,0 +1,21 @@
+package org.hyeonjoon.gcu.global.security.auditing;
+
+import lombok.NonNull;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuditingAware implements AuditorAware<String> {
+    @Override @NonNull
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+        return Optional.of(authentication.getName());
+    }
+}
