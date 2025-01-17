17296dc1f82d1ca3f0fc02fbab3a5dade1864db2
hyeonjoonpark <pjjoon1379@gmail.com>
2025-01-17 10:20:19 +0900

    #9 UserRepository 생성

---
+++ src/main/java/org/hyeonjoon/gcu/domain/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.hyeonjoon.gcu.domain.auth.repository;
+
+import org.hyeonjoon.gcu.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    User findByUsername(String username);
+}
