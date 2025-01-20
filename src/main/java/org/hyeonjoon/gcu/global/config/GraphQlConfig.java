dc70550684f6d2c3e3e8a67bddb0634d032d7097
hyeonjoonpark <pjjoon1379@gmail.com>
2025-01-20 09:58:31 +0900

    #21 add GraphQL Scalar type

---
+++ src/main/java/org/hyeonjoon/gcu/global/config/GraphQlConfig.java
@@ -0,0 +1,21 @@
+package org.hyeonjoon.gcu.global.config;
+
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQlConfig {
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return
+                wiringBuilder -> wiringBuilder
+                        .scalar(ExtendedScalars.DateTime)
+                        .scalar(ExtendedScalars.Json)
+                        .scalar(ExtendedScalars.LocalTime)
+                        .scalar(ExtendedScalars.Object)
+                        .scalar(ExtendedScalars.UUID)
+                        .scalar(ExtendedScalars.Url);
+    }
+}
